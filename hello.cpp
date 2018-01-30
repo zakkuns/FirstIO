e85797e1b2e3c3b744090fe3b417e0f005844a24
zakkuns <zkuns@mail.csuchico.edu>
2018-01-30 11:16:13 -0800

    Changes and greeting

--- hello.cpp
+++ hello.cpp
@@ -4,6 +4,8 @@
 #include <string>
 
 int main() {
+
+    std::cout<<"Hi my name is Zakary. Whats yours?"<<std::endl;
     std::string input;
     std::cout << "What's your first name?" << std::endl;
     std::cin >> input;
